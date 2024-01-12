3c43aa639c1102182b3685cb922526047281bcf6
Fajrul Fahmi <95855377+fajrulfahmi@users.noreply.github.com>
2024-01-12 10:36:37 +0700

    Add files via upload

---
+++ MPU-6050_II/MPU-6050_II.ino
@@ -0,0 +1,25 @@
+#include <MPU6050_tockn.h>
+#include <Wire.h>
+
+MPU6050 mpu6050(Wire);
+
+long timer = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
+}
+
+void loop() {
+  mpu6050.update();
+
+  if(millis() - timer > 10){
+    Serial.print("angleX : ");Serial.print(mpu6050.getAngleX());
+    Serial.print("\tangleY : ");Serial.print(mpu6050.getAngleY());
+    Serial.print("\tangleZ : ");Serial.println(mpu6050.getAngleZ());
+    Serial.println("");
+    timer = millis();
+  }
+}
